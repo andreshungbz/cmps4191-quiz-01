968b54737b7fbd54d1141df5f73f0f592aea1cc6
Andres Hung <2018118240@ub.edu.bz>
2026-08-27 19:49:20 +0000

    docs: Revise comments for errors.go

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,8 +5,7 @@ import (
 	"net/http"
 )
 
-// logError writes server-side error messages. It records the error
-// and HTTP request method and URI.
+// logError writes server-side error messages using the application's slog.Logger.
 func (app *application) logError(r *http.Request, err error) {
 	var (
 		method = r.Method
@@ -16,10 +15,9 @@ func (app *application) logError(r *http.Request, err error) {
 	app.logger.Error(err.Error(), "method", method, "uri", uri)
 }
 
-// errorResponse writes error messages to the client in JSON.
+// errorResponse sends JSON error messages to the client.
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
 	env := envelope{"error": message}
-
 	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
 		app.logError(r, err)
@@ -27,34 +25,31 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 	}
 }
 
-// serverErrorResponse sends a generic error message in JSON indicating
-// a problem with the server.
+// serverErrorResponse logs a generic server error message and sends it to the client.
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
-
-	message := "The server encountered a problem and could not process your request"
+	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
-// badRequestResponse sends a 400 HTTP status code, typically for invalid JSON.
+// badRequestResponse sends a 400 HTTP status code response to the client.
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
 
-// notFound Response sends a 404 HTTP status code.
+// methodNotAllowedResponse sends a 405 HTTP status code response to the client.
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+// notFoundResponse sends a 404 HTTP status code response to the client.
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
-	message := "The requested resource could not be found"
+	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
-// failedValidationResponse sends a 422 HTTP status code, typically for
-// validly structured JSON whose field values are not valid.
+// failedValidationResponse sends a 422 HTTP status code response to the client.
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
-}
-
-// methodNotAllowedResponse sends a 405 HTTP status code.
-func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("The %s method is not supported for this resource", r.Method)
-	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
