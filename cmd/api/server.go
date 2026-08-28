e082ac3afe15102439e5d5b2212f6c9103875191
Andres Hung <2018118240@ub.edu.bz>
2026-08-28 04:00:54 +0000

    docs: Revise comments in server.go

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -12,11 +12,9 @@ import (
 	"time"
 )
 
-// serve starts the HTTP server and monitors for shutdown signals so that the server
-// and background tasks can gracefully terminate.
+// serve starts the application HTTP server.
 func (app *application) serve() error {
 	// HTTP Server Configuration
-
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		Handler:      app.routes(),
@@ -26,53 +24,49 @@ func (app *application) serve() error {
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
-	// Goroutine for gracefully shutting down HTTP Server on SIGINT (Ctrl + C) and SIGTERM (pkill)
-	// and finishing any background tasks
-
+	// Goroutine for gracefully shutting down HTTP Server on SIGINT (Ctrl + C) and SIGTERM (pkill),
+	// finishing any background tasks, and cancelling any ongoing worker routines.
 	shutdownError := make(chan error)
 	go func() {
-		// create a single buffered channel that blocks until a signal is received
+		// Use a single buffered channel that blocks until a signal is received.
 		quit := make(chan os.Signal, 1)
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		s := <-quit
-
+		s := <-quit // Block here until signal is caught.
 		app.logger.Info("shutting down server due to caught signal", "signal", s.String())
 
-		// allow HTTP server to close any remaining connections with the configured timeout period
+		// Allow HTTP server to close any remaining connections.
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 		err := srv.Shutdown(ctx)
 		if err != nil {
 			shutdownError <- err
 		}
-
+		
+		// BACKGROUND TASKS
 		app.logger.Info("completing background tasks", "addr", srv.Addr)
 
+		// Report worker goroutine
 		if app.workerCancel != nil {
 			app.workerCancel()
 		}
 
-		// block until all goroutines are finished
-		app.wg.Wait()
+		app.wg.Wait() // Block here until all background tasks are finished.
 		shutdownError <- nil
 	}()
 
 	// Run HTTP Server
-
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
-
-	// we should expect the ErrServerClosed error since Shutdown was called.
 	err := srv.ListenAndServe()
+	// NOTE: ErrServerClosed is expected when Shutdown is called in the preceding goroutine.
 	if !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
 
-	// block until shutdown signal is received
-	err = <-shutdownError
+	// Shutdown HTTP Server
+	err = <-shutdownError // Block until shutdown signal is received.
 	if err != nil {
 		return err
 	}
-
 	app.logger.Info("stopped server", "addr", srv.Addr)
 
 	return nil
